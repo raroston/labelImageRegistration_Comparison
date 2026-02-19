4dba57211b8f74004ece4834703973ab2d96c88c
raroston <raroston@gmail.com>
2026-02-19 02:53:48 +0000

    Updated script

--- Scripts/segment_vol_dice_analysis.R
+++ Scripts/segment_vol_dice_analysis.R
@@ -18,6 +18,7 @@ labelnames = rbind(c(0, "background"), labelnames)
 names(labelnames) <- c("Label", "Organ")
 labelnames$OrganName = gsub("_left", " (left)", labelnames$Organ)
 labelnames$OrganName = gsub("_right", " (right)", labelnames$OrganName)
+labelnames$OrganName = gsub("_", " ", labelnames$OrganName)
 
 metadata = read.csv("./Data/scan_genotypes.csv")
 ko = which(metadata$Genotype == "MUT/MUT")
@@ -30,25 +31,7 @@ ma.labels = lapply(metadata$ScanID, function(X){antsImageRead(dir(ma.label.dir, 
 io.labels = lapply(metadata$ScanID, function(X){antsImageRead(dir(io.label.dir, pattern = paste0(X,".*step2"), full.names = T, recursive = T))})
 li.labels = lapply(metadata$ScanID, function(X){antsImageRead(dir(li.label.dir, pattern = paste0(X,".*step2"), full.names = T, recursive = T))})
 
-
-ma.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], labelStats(imgs[[X]], ma.labels[[X]]))))
-io.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], labelStats(imgs[[X]], io.labels[[X]]))))
-li.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], labelStats(imgs[[X]], li.labels[[X]]))))
-
-volumes = data.frame(LabelType = rep(c("m", "i", "l"), each = nrow(ma.vols)),
-                     rbind(ma.vols, io.vols, li.vols))
-volumes$GenotypeLabel = paste0(volumes$Subdirectory, "-", volumes$LabelType)
-
-
-
-
-organVolPlot <- ggplot(volumes, aes(x = GenotypeLabel, y = Volume)) +
-             geom_boxplot() +
-             theme_bw() +
-             theme(legend.position = "none") +
-             facet_wrap(~LabelValue, nrow = 4, ncol = 4, scales = "free", strip.position = "bottom", shrink = F) +
-             theme(text = element_text(size = 4))
-ggsave("./Results/organVols_violinPlot_2.tif", organVolPlot)
+# Dice
 
 
 io.overlap = do.call(rbind, lapply(1:length(ma.labels), function(X) data.frame(metadata[X,], RegType = "Intensity Only", labelOverlapMeasures(ma.labels[[X]], io.labels[[X]]))))
@@ -56,9 +39,6 @@ li.overlap = do.call(rbind, lapply(1:length(ma.labels), function(X) data.frame(m
 
 diceCoeff.table = rbind(io.overlap, li.overlap)
 diceCoeff.table$Subdirectory <- relevel(factor(diceCoeff.table$Subdirectory), ref = "WT")
-diceCoeff.table$GenotypeRegtype <- factor(paste0(diceCoeff.table$Subdirectory, "\n", diceCoeff.table$RegType),
-                                          levels = c("WT\nIO", "KO\nIO", "WT\nLI", "KO\nLI"), 
-                                          ordered = T)
 for(i in 1:nrow(diceCoeff.table)){
   if(diceCoeff.table$Label[i] == "All") {
     diceCoeff.table$OrganName[i] <- "All"
@@ -68,32 +48,42 @@ for(i in 1:nrow(diceCoeff.table)){
 }
 diceCoeff.table$OrganName <- relevel(factor(diceCoeff.table$OrganName), ref = "All")
 
-
-DicePlot1<-ggplot(diceCoeff.table, aes(x = GenotypeRegtype, y = MeanOverlap)) +
-  geom_boxplot() +
-  ylim(0,1) +
-  labs(x = "", y = "Dice") +
-  theme_bw() +
-  theme(legend.position = "none") +
-  facet_wrap(~OrganName, nrow = 4, ncol = 4, scales = "fixed", 
-             strip.position = "top", 
-             shrink = F, 
-             axes = "margins", 
-             axis.labels = "margins") +
-  theme(text = element_text(size = 9)) +
-  theme(axis.text.x = element_text(hjust = 0.5))
-ggsave("./Results/dicePlot_test.tif", DicePlot1)
-
-
 DicePlot <- ggplot(diceCoeff.table, aes(x = OrganName, y = MeanOverlap, color = OrganName)) +
-  geom_boxplot() +
-  scale_color_grey() +
   theme_bw() +
+  geom_boxplot(linewidth = 0.3, color = "black") +
   labs(x = "", y = "Dice") +
   facet_grid(cols = vars(Subdirectory), rows = vars(RegType), switch = "both") +
-  theme(legend.position = "right",
+  theme(legend.position = "none",
         text = element_text(size = 9),
-        legend.key.size = unit(0.4, "cm"),
-        axis.text.x = element_blank(),      # Remove x-axis text
-        axis.ticks.x = element_blank()) 
-ggsave("./Results/dice_test2.tif", DicePlot)
+        axis.text.x = element_text(margin=margin(t=0.5),angle = 45, vjust = 1, hjust = 1),      # Remove x-axis text
+        #axis.ticks.x = element_blank()
+  )
+ggsave("./Results/dice_plot.tif", DicePlot, width = 5, height = 4, dpi = 300)
+
+
+# Volume
+
+ma.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], RegType = "M", labelStats(imgs[[X]], ma.labels[[X]]))))
+io.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], RegType = "IO", labelStats(imgs[[X]], io.labels[[X]]))))
+li.vols = do.call(rbind, lapply(1:length(imgs), function(X) data.frame(metadata[X,], RegType = "LI", labelStats(imgs[[X]], li.labels[[X]]))))
+
+volumes = rbind(ma.vols, io.vols, li.vols)
+for(i in 1:nrow(volumes)){
+    volumes$OrganName[i] <- labelnames[which(labelnames$Label == volumes$Label[i]), "OrganName"]
+}
+volumes = volumes[which(volumes$LabelValue !=0),]
+volumes$GenotypeRegtype = factor(paste0(volumes$Subdirectory, "-", volumes$RegType),
+                                 levels = c("WT-M", "WT-IO", "WT-LI", "KO-M", "KO-IO", "KO-LI"),
+                                 ordered = T)
+
+organVolPlot <- ggplot(volumes, aes(x = GenotypeRegtype, y = Volume)) +
+  theme_bw() +
+  geom_boxplot(linewidth = 0.3, color = "black") +
+  labs(x = "", y = expression("Organ Volume (mm"^3*")")) +
+  facet_wrap(~OrganName, nrow = 3, ncol = 5, scales = "free", strip.position = "bottom", shrink = F) +
+  theme(legend.position = "none",
+        text = element_text(size = 9),
+        axis.text.x = element_text(margin=margin(t=0.5),angle = 45, vjust = 1, hjust = 1),      # Remove x-axis text
+        #axis.ticks.x = element_blank()
+  )
+ggsave("./Results/organVols_plot.tif", organVolPlot, width = 7, height = 5, dpi =300)
